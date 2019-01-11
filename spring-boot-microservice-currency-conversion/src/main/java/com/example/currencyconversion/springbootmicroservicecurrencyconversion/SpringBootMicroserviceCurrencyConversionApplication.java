a94c4e793f3dc916048fda5ac04e5e9715216583
Teodora Maria <teodoramaria0112@gmail.com>
2019-01-11 18:19:22 +0200

    Enabled Feign instead of Rest Template

--- spring-boot-microservice-currency-conversion/src/main/java/com/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ spring-boot-microservice-currency-conversion/src/main/java/com/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -2,8 +2,12 @@ package com.example.currencyconversion.springbootmicroservicecurrencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.example.currencyconversion")
+@EnableDiscoveryClient
 public class SpringBootMicroserviceCurrencyConversionApplication {
 
     public static void main(String[] args) {
